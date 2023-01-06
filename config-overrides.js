f402d96725a774b6c3a428ec7ef7286e231050b6
Coder1603 <congductp1@gmail.com>
2023-01-06 11:20:25 +0700

    commit test

--- config-overrides.js
+++ config-overrides.js
@@ -1,3 +1,7 @@
-const { override, useBabelRc } = require("customize-cra");
+module.exports = function override(config, env) {
+    //do stuff with the webpack config...
+    return config;
+};
+const { override, useBabelRc } = require('customize-cra');
 
 module.exports = override(useBabelRc());
--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
-import "./App.css";
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from '~/routes';
+import { DefaultLayout } from '~/components/Layout';
 
 function App() {
-  return <div className="App"></div>;
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        const Page = route.component;
+
+                        let Layout = DefaultLayout;
+                        if (route.layout === null) {
+                            Layout = Fragment;
+                        } else if (route.layout) {
+                            Layout = route.layout;
+                        }
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            ></Route>
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
